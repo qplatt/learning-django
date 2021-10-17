8badfc27a2e6ca8b7b03cfc4642b961e1e91111d
qplatt <quincyplatt@gmail.com>
2021-10-17 17:34:33 +0000

    Fixed typo

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.enviorn.get('SECERET_KEY', 'django-insecure-9^b)4ywnyxvlu#51_e#2mguij3p4&lfnzo2l&=843$!0@nze=m')
+SECRET_KEY = os.environ.get('SECERET_KEY', 'django-insecure-9^b)4ywnyxvlu#51_e#2mguij3p4&lfnzo2l&=843$!0@nze=m')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.enviorn.get('HEROKU_HOSTNAME')]
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
@@ -84,7 +84,8 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse(os.enviorn.get('DATABASE_URL'))
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+
 }
 
 
